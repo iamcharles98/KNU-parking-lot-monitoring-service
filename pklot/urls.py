6bc311b224b96b41838ec74e0114594e495551c2
meeeeju <meeju06@gmail.com>
2023-05-23 20:13:07 +0900

    add detail pages and do rendering

--- pklot/urls.py
+++ pklot/urls.py
@@ -8,8 +8,15 @@ urlpatterns = [
 
     path("", views.home),
     path("cover",views.cover),
-    path("section4",views.section_4,name="section_4"),
-    # path('', home, name='home'),
-    # path('detail/<str:id>',detail, name="detail"),
-    path("temp/<str:id>",views.temp,name="temp")
+    path("section4",views.section_4,name="section_4"), # url은 html 에서 name 으로 불려진다.
+
+    path("detail4D",views.detail4_D,name="detail4_D"),
+    path("detail4H",views.detail4_H,name="detail4_H"),
+    path("detail4G",views.detail4_G,name="detail4_G"),
+    path("detail4F",views.detail4_F,name="detail4_F"),
+    
+    path("temp/<str:id>",views.temp,name="temp"),
+
+    
+
 ]
